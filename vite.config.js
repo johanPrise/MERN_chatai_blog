27f63e1a0f6c8dc0d01f3dbe6a2cc8040e908872
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-07-04 17:04:37 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig(({ command, mode }) => {
         '/api': {
           target: process.env.VITE_API_URL && process.env.VITE_API_URL.trim() !== "" 
                 ? process.env.VITE_API_URL 
-                  : 'https://mern-backend-whej.onrender.com',
+                  : 'https://mern-backend-neon.vercel.app/',
               changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '')
         }
