b0a371b1a5695a527aa972d845a4e26dbcad3ccf
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-07-05 17:21:38 +0100

    Update createPost.tsx

--- src/pages/createPost.tsx
+++ src/pages/createPost.tsx
@@ -111,9 +111,7 @@ useEffect(() => {
     'link',
     'image',
   ];
-
-    return (
-if (!isAuthorOrAdmin) {
+    if (!isAuthorOrAdmin) {
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg">
@@ -125,6 +123,8 @@ if (!isAuthorOrAdmin) {
     </div>
   );
 }else{
+
+    return (
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-2xl">
         <h1 className="text-center text-2xl font-bold text-lime-600 sm:text-3xl">
@@ -200,8 +200,8 @@ if (!isAuthorOrAdmin) {
           </button>
         </form>
       </div>
-    </div>
-});
+    </div>);
+}
 }
 ;
 
