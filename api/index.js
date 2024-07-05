97c05cff37849e64a8269e9cd6594bf86bc1f644
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-07-05 14:03:09 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -123,7 +123,9 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Configurez le transporteur de nodemailer pour envoyer des emails
