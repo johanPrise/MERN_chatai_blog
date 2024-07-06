81ceccec09aa9e210c539f2aa7ca6910a1ee8e00
Johan <prisojohan2@gmail.com>
2024-07-06 20:17:06 +0100

    sss

--- api/index.js
+++ api/index.js
@@ -398,7 +398,7 @@ app.post("/logout/", (req, res) => {
 
 // Définir une route pour la création d'un nouveau post
 // Route pour créer un post
-app.post('/post', authMiddleware, upload.single('file'), async (req, res) => {
+app.post('/post', upload.single('file'), async (req, res) => {
   const token = req.cookies.token;
   
   jwt.verify(token, secret, {}, async (err, info) => {
