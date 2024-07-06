2279b7faad5201bcbb3c0706f4d602c8b2604946
Johan <prisojohan2@gmail.com>
2024-07-06 18:34:19 +0100

    update createpost

--- src/pages/createPost.tsx
+++ src/pages/createPost.tsx
@@ -51,7 +51,7 @@ useEffect(() => {
       formData.append('file', file);
 
       try {
-        const response = await fetch('/post', {
+        const response = await fetch('api/post', {
           method: 'POST',
           body: formData,
           credentials: 'include',
@@ -84,7 +84,7 @@ const handleSubmit = async (event) => {
 
  
 
-    const response = await fetch('/post', {
+    const response = await fetch('api/post', {
       method: 'POST',
       body: formData,
       credentials: 'include',
