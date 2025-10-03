853edd0e47675eb46653145932f9d42053f4aaa0
Johan Priso <prisojohan2@gmail.com>
2025-10-03 18:00:36 +0200

    refactor(types): enhance Fastify and Mongoose typings
    - Extend FastifyBaseLogger interface with multiple logging method overloads
    - Add user property to FastifyRequest interface with user details
    - Declare Mongoose Types.ObjectIdConstructor with isValid method typing
    - Update tsconfig.json to include additional type declaration files under src/types
    - Remove deprecated @fastify/jwt module augmentation in favor of native Fastify typings

--- api-fastify/src/types/fastify.d.ts
+++ api-fastify/src/types/fastify.d.ts
@@ -1,18 +1,36 @@
-import '@fastify/jwt';
+import 'fastify';
+import { Types } from 'mongoose';
 
-declare module '@fastify/jwt' {
-  interface FastifyJWT {
-    payload: {
-      _id: string;
-      email: string;
-      username: string;
-      role: string;
-    };
+declare module 'fastify' {
+  interface FastifyBaseLogger {
+    error(msg: string, ...args: unknown[]): void;
+    error(obj: object, msg?: string, ...args: unknown[]): void;
+    warn(msg: string, ...args: unknown[]): void;
+    warn(obj: object, msg?: string, ...args: unknown[]): void;
+    info(msg: string, ...args: unknown[]): void;
+    info(obj: object, msg?: string, ...args: unknown[]): void;
+    debug(msg: string, ...args: unknown[]): void;
+    debug(obj: object, msg?: string, ...args: unknown[]): void;
+    trace(msg: string, ...args: unknown[]): void;
+    trace(obj: object, msg?: string, ...args: unknown[]): void;
+    fatal(msg: string, ...args: unknown[]): void;
+    fatal(obj: object, msg?: string, ...args: unknown[]): void;
+  }
+
+  interface FastifyRequest {
     user: {
       _id: string;
       email: string;
       username: string;
       role: string;
     };
+  }
+}
+
+declare module 'mongoose' {
+  namespace Types {
+    interface ObjectIdConstructor {
+      isValid(id: string | number | Types.ObjectId | null | undefined): boolean;
+    }
   }
 }
